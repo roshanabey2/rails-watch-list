7a4f2c6b03675b862392928f04aaffc479a1d73b
Roshan Abeysekera <roshanabey2@gmail.com>
2025-01-09 20:18:31 +0000

    Models are fine

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ gem "font-awesome-sass", "~> 6.1"
 gem "simple_form"
 gem "sassc-rails"
 gem 'sprockets-rails', :require => 'sprockets/railtie'
+gem 'faker'
+gem 'hirb'
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+genres = [
+  "Action", "Adventure", "Comedy", "Drama", "Fantasy",
+  "Horror", "Mystery", "Romance", "Sci-Fi", "Thriller"
+]
+ 10.times do
+  p "Creating Movie List"
+  list = List.new({ name: genres.sample })
+  list.save
+  genres.delete(list.name)
+  p "Created #{list.name} List"
+end
